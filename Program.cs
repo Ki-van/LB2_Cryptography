4f7449879328557701b88cf136e229d38585a3ff
Ki-van <kirill.ivanin00@yandex.ru>
2021-03-16 04:28:01 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LB2_Cryptography
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
