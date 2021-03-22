422276b6c47ba4279706c03f0b39379d13589cfe
Ki-van <kirill.ivanin00@yandex.ru>
2021-03-22 22:58:43 +0300

    fix bug with opening an opened file for read

--- Crypto.cs
+++ Crypto.cs
@@ -10,9 +10,9 @@ namespace LB2_Cryptography
     class Crypto
     {
         private static string alphabet = "абвгдеёжзийклмнопрстуфхцчшщъьыэюя_,.АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЭЮЯ ";
-       //
-       //private static int m = alphabet.Length;
-        private static List<char []> ToTableLineByLine(string path, int columnNumber)
+        //
+        //private static int m = alphabet.Length;
+        private static List<char[]> ToTableLineByLine(string path, int columnNumber)
         {
             StreamReader input = new StreamReader(path, Encoding.UTF8);
             string sourceLine;
@@ -50,7 +50,7 @@ namespace LB2_Cryptography
         {
             if (!Crypto.CheckBlockTranspositionCipherKey(columnNumber, order))
                 return null;
-
+            
             StreamReader input = new StreamReader(path, Encoding.UTF8);
 
             string sourceLine;
@@ -88,7 +88,7 @@ namespace LB2_Cryptography
             return strings;
         }
 
-        private static bool CheckBlockTranspositionCipherKey(int blockLen, int [] key)
+        private static bool CheckBlockTranspositionCipherKey(int blockLen, int[] key)
         {
             if (blockLen > 0)
             {
@@ -120,7 +120,7 @@ namespace LB2_Cryptography
             while ((line = input.ReadLine()) != null)
             {
                 line = line.Trim();
-                if (line.Length > 0) 
+                if (line.Length > 0)
                 {
                     for (int i = 0; i < line.Length; i++)
                     {
@@ -133,11 +133,11 @@ namespace LB2_Cryptography
             inputBase.Position = 0;
 
             string[] words = new string[wordCount];
-            for(int i = 0; i < words.Length; i++)
+            for (int i = 0; i < words.Length; i++)
             {
                 line = input.ReadLine().Trim();
-                
-                for(int j = 0; j < line.Length; j++)
+
+                for (int j = 0; j < line.Length; j++)
                 {
                     if (line[j] != ' ')
                         words[i] += line[j];
@@ -148,7 +148,7 @@ namespace LB2_Cryptography
             input.Close();
 
             return words;
-        }   
+        }
 
         public static int GetFileLenghtInLetters(string path)
         {
@@ -181,78 +181,80 @@ namespace LB2_Cryptography
             return (false, left);
         }
 
-        public static int[] HackColumnarTranspositionCipher(string path, string dictionaryPath, double match = 0.7, 
+        public static int[] HackColumnarTranspositionCipher(string path, string dictionaryPath, double match = 0.7,
             string outputPath = "decrypted.txt", int maxKeyLen = 50)
         {
             bool compare(int val_1, int val_2)
             {
                 if (val_1 > val_2)
-                    return true;
-                else
                     return false;
+                else
+                    return true;
             }
-            
+
             int[] key = null;
-            try
+
+            StreamReader decrypted;
+            string[] dictionary = ToWords(dictionaryPath);
+            for (int keyLen = 2; keyLen <= maxKeyLen; keyLen++)
             {
-                StreamReader decrypted = new StreamReader(outputPath, Encoding.UTF8);
-                string[] dictionary = ToWords(dictionaryPath);
-                for (int keyLen = 2; keyLen <= maxKeyLen; keyLen++)
+                key = new int[keyLen];
+                for (int i = 0; i < keyLen; i++)
+                    key[i] = i + 1;
+
+                do
                 {
-                    key = new int[keyLen];
-                    for (int i = 0; i < keyLen; i++)
-                        key[i] = i + 1;
-                    do
+                    Console.Write("Check key: ");
+                    for (int i = 0; i < key.Length; i++)
+                        Console.Write(key[i] + " ");
+                    Console.WriteLine();
+
+                    Crypto.DecryptColumnarTranspositionCipher(path, keyLen, key);
+
+                    string decryptedLine, word = "";
+                    double actualMatch = 0, wordCount = 0;
+                    decrypted = new StreamReader(outputPath, Encoding.UTF8);
+                    while ((decryptedLine = decrypted.ReadLine()) != null)
                     {
-                        Crypto.DecryptColumnarTranspositionCipher(path, keyLen, key);
-                        decrypted.BaseStream.Position = 0;
-                        
-                        string decryptedLine, word = "";
-                        double actualMatch = 0, wordCount = 0;
-                        while((decryptedLine = decrypted.ReadLine()) != null)
+                        decryptedLine = decryptedLine.Trim().ToLowerInvariant();
+                        for (int i = 0; i < decryptedLine.Length; i++)
                         {
-                            decryptedLine = decryptedLine.Trim();
-                            for(int i = 0; i < decryptedLine.Length; i++)
+                            while (i < decryptedLine.Length && decryptedLine[i] != ' ')
                             {
-                                while (decryptedLine[i] != ' ' && i < decryptedLine.Length)
-                                {
-                                    word += decryptedLine[i];
-                                    i++;
-                                }
+                                word += decryptedLine[i];
                                 i++;
-                                wordCount++;
-                                if (BinarySearch<string>(dictionary, word).Item1)
-                                    actualMatch++;
-
                             }
-                        }
-                        actualMatch = wordCount / actualMatch;
+                            i++;
+                            wordCount++;
+                            if (BinarySearch<string>(dictionary, word).Item1)
+                                actualMatch++;
 
-                        if (actualMatch >= match)
-                        {
-                            Console.WriteLine("Key:");
-                            for (int i = 0; i < key.Length; i++)
-                                Console.Write(key[i] + " ");
-                            Console.WriteLine("Press 'y' to continue");
-                            char c = (char)Console.Read();
-
-                            if (c != 'y')
-                                break;
-                            else
-                                Console.WriteLine("Key found, check decrypted.txt");
                         }
+                    }
+                    actualMatch /= wordCount ;
+                    decrypted.Close();
+
+                    if (actualMatch >= match)
+                    {
+                        Console.WriteLine("Key:");
+                        for (int i = 0; i < key.Length; i++)
+                            Console.Write(key[i] + " ");
+                        Console.WriteLine("Press 'y' to continue");
+                        char c = (char)Console.Read();
+
+                        if (c != 'y')
+                            break;
                         else
-                            actualMatch = 0;
-                        
-                    } while (Narayana.NextPermutation<int>(key, compare));
-                    
-                }
-                Console.WriteLine("Faild to find a key");
-            }catch(Exception e)
-            {
-                Console.WriteLine(e.Message);
-                return null;
+                            Console.WriteLine("Key found, check decrypted.txt");
+                    }
+                    else
+                        actualMatch = 0;
+
+                } while (Narayana.NextPermutation<int>(key, compare));
+
             }
+            Console.WriteLine("Faild to find a key");
+
             return key;
         }
 
@@ -274,7 +276,7 @@ namespace LB2_Cryptography
             result = result && Crypto.DecryptColumnarTranspositionCipher("tmp.txt", columnNumber, key);
 
             FileInfo fTmp = new FileInfo("tmp.txt");
-          //  fTmp.Delete();
+            fTmp.Delete();
 
             return result;
         }
@@ -286,6 +288,7 @@ namespace LB2_Cryptography
                 return false;
 
             StreamWriter output = new StreamWriter(outputPath, false, Encoding.UTF8);
+            output.AutoFlush = true;
 
             char[,]? strings = Crypto.ToTableColumnByColumn(path, columnNumber, key);
             if (strings == null)
@@ -305,13 +308,13 @@ namespace LB2_Cryptography
                         outputLine += strings[i, j];
                 }
 
-                output.WriteLine(outputLine);
+                output.Write(outputLine);
             }
-
+            
             output.Close();
             return true;
         }
-        public static bool EncryptColumnarTranspositionCipher(string path, int columnNumber, 
+        public static bool EncryptColumnarTranspositionCipher(string path, int columnNumber,
             int[] key, string outputPath = null)
         {
             if (!Crypto.CheckBlockTranspositionCipherKey(columnNumber, key))
@@ -365,19 +368,20 @@ namespace LB2_Cryptography
 
             string sourceBlock = "", outputBlock;
             int sourceBlockI = 0;
-            for(int i = 0; i < sourceText.Length; i++)
+            for (int i = 0; i < sourceText.Length; i++)
             {
-                if(sourceBlockI < blockLen)
+                if (sourceBlockI < blockLen)
                 {
                     if (alphabet.Contains(sourceText[i]))
                     {
                         sourceBlock += sourceText[i];
                         sourceBlockI++;
                     }
-                } else
+                }
+                else
                 {
                     outputBlock = "";
-                    for(int j = 0; j < blockLen; j++)
+                    for (int j = 0; j < blockLen; j++)
                     {
                         outputBlock += sourceBlock[key[j] - 1];
                     }
@@ -390,13 +394,13 @@ namespace LB2_Cryptography
                 }
             }
 
-            if(sourceBlockI < blockLen)
+            if (sourceBlockI < blockLen)
             {
                 for (; sourceBlockI < blockLen; sourceBlockI++)
                 {
                     sourceBlock += ".";
                 }
-            } 
+            }
 
             outputBlock = "";
             for (int j = 0; j < blockLen; j++)
@@ -422,7 +426,7 @@ namespace LB2_Cryptography
             char[,]? strings = Crypto.ToTableColumnByColumn(path, columnNumber, order);
             if (strings == null)
                 return false;
-            
+
             int stringNumber = strings.Length / columnNumber;
 
             string outputLine;
